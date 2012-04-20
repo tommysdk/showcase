caa31b64e8c706832871b3d393c7bbc5a5b3dbf7
Tommy Tynja <tommy.tynja@diabol.se>
2012-04-20 06:47:23 +0200

    Added week service (EJB 3.1 session bean) with associated test

---
+++ arquillian/src/test/java/tommysdk/showcase/arquillian/misc/WeekServiceTest.java
@@ -0,0 +1,36 @@
+package tommysdk.showcase.arquillian.misc;
+
+import junit.framework.Assert;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import javax.ejb.EJB;
+import java.util.Calendar;
+
+/**
+ * @author Tommy Tynj&auml;
+ */
+    @RunWith(Arquillian.class)
+    public class WeekServiceTest {
+
+        @Deployment
+        public static Archive deployment() {
+            return ShrinkWrap.create(JavaArchive.class, "helloworld.jar")
+                    .addClass(WeekService.class);
+        }
+
+        @EJB(mappedName = "java:module/WeekService")
+        WeekService service;
+
+        @Test
+        public void shouldReturnGreeting() {
+            Assert.assertNotNull(service.weekOfYear());
+            Assert.assertEquals("" + Calendar.getInstance().get(Calendar.WEEK_OF_YEAR),
+                    service.weekOfYear());
+        }
+    }
